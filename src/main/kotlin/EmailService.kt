49feb858e47cacb973d83165cb6f0d7d88fd74e7
sveta.chuinyshena <sveta.chuinyshena@gmail.com>
2021-05-04 19:44:17 +0300

    code review fixes

--- src/main/kotlin/EmailService.kt
+++ src/main/kotlin/EmailService.kt
@@ -1,32 +1,67 @@
 import kotlinx.coroutines.*
 
-fun sendEmail(address: String, message: String) {
-    println("Sent '$$message' to $address")
+data class User(
+    val name: String,
+    val surname: String,
+    val age: Int
+)
+
+data class UserInfo(
+    val telephoneNumber: String,
+    val address: String
+)
+
+data class AggregatedUserData(
+    val user: User,
+    val info: UserInfo?
+)
+
+class UserService {
+    private val users = listOf(
+        User("Vasya", "Pupkin", 25),
+        User("Olya", "Limonova", 40),
+        User("Andrey", "Latushev", 20),
+        User("Alexey", "Ivanov", 63)
+    ).withIndex().associate { it.index to it.value }
+
+    suspend fun getUser(id: Int): User {
+        delay(3000)
+
+        return users[id] ?: error("User with id: $id not found")
+    }
 }
 
-suspend fun getAddress(): String {
-    delay(3000)
+class UserInfoService  {
+    private val usersInfo = listOf(
+        UserInfo("8-921-000-00-00", "Кантемировская ул. д.2")
+    ).withIndex().associate { it.index to it.value }
 
-    println("Got email address")
-    return "coroutine@kotlin.org"
+    suspend fun getUserInfo(id: Int): UserInfo? {
+        delay(3000)
+
+        return usersInfo[id]
+    }
 }
 
-suspend fun getMessage(): String {
-    delay(5000)
+class UserDataAggregationService(
+    private val userService: UserService,
+    private val userInfoService: UserInfoService
+) {
+    suspend fun aggregateUserData(id: Int): AggregatedUserData = coroutineScope {
+        val user = async { userService.getUser(id) }
+        val userInfo = async { userInfoService.getUserInfo(id) }
 
-    println("Email body has been created")
-    return "Hey there!"
-}
-
-fun CoroutineScope.sendEmailSuspending() = launch {
-    val message = async { getMessage() }
-    val address = async { getAddress() }
-
-    println("Waiting for email data")
-
-    sendEmail(address.await(), message.await())
+        AggregatedUserData(user.await(), userInfo.await())
+    }
 }
 
 fun main() {
-    runBlocking { sendEmailSuspending() }
+    runBlocking {
+        val userService = UserService()
+        val infoService = UserInfoService()
+        val aggregatedUserService = UserDataAggregationService(userService, infoService)
+
+        println(aggregatedUserService.aggregateUserData(0))
+        println(aggregatedUserService.aggregateUserData(1))
+    }
 }
